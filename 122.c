2f35d6038e0ce5af7222891201614e21f23ec06b
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-11 23:20:18 +0530

    Create 122.c

---
+++ 122.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+	int num,rem,temp,rev = 0;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	temp = num;
+	while(num != 0)
+  {
+		rem = num % 10;
+		rev = (rev*10) +rem;
+		num = num/10;
+	}
+	if(temp == rev)
+		printf("Palindrome\n!");
+	else
+		printf("Not a palindrome\n!");
+	
+}
