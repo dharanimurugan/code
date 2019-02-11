71f18c90c77a3373ac9c21604607bd616ac6fd67
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-11 23:32:44 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,c,d,i,count;
+printf("enter the two limits");
+scanf("%d%d",&c,&d);
+for(i=c;i<d;i++)
+{
+count=0;
+for(n=2;n<i;n++)
+{
+if(i%n==0)
+count++;
+}
+if(count==0)
+printf("%d\n",i);
+}
+}
