cc5d0cbb406bcffb45e531cb195c7a3f6c867cde
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-11 23:42:33 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+void main(void)
+{
+int n=0,rem,result=0,r1,r2,temp1,temp2,i;
+scanf("%d%d",&r1,&r2);
+for(i=r1+1;i<r2;++i)
+{
+	temp1=i;
+	temp2=i;
+while(temp1!=0)
+{
+	temp1=temp1/10;
+	++n;
+}
+while(temp2!=0)
+{
+	rem=temp2%10;
+	result=result+pow(rem,n);
+	temp2=temp2/10;
+}
+if(result==i)
+{
+printf("%d",i);
+}
+n=0;
+result=0;
+}
+}
