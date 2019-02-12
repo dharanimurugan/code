902afcccf80cbdcdf85f8cfa1cd250919d028607
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-12 21:36:23 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,15 @@
+#include<stdio.h>        
+void main()
+{
+    int n,i;
+    int b=1,c=1,d;
+    scanf("%d",&n);
+    printf("%d  %d  ",b,c);
+    for(i=2;i<n;i++)
+    {
+        c=a+b;
+        printf("%d  ",d);
+        b=c;
+        c=d;
+    }
+}
