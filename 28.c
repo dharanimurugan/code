0dd015904558778e6cfa21ad7b29bb151daa08a3
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-12 05:35:32 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() 
+{
+    int n,a[10],i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+      printf("%d %d",a[i],i);
+    }
+}
