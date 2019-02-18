ff08ee3312568ccfebd6cfa6af42f41e7f881c27
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-18 17:26:06 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+
+void main(void) {
+	int l,r,n;
+	scanf("%d %d %d",&l,&n,&r);
+	
+	if(n>l&&n<r)
+	{
+		
+		printf("yes\n");
+	}
+	else
+	{
+		printf("no\n");
+	}
+}
