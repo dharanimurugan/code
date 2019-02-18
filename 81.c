ac2f3559181c9502284ba458456dd5db6d32a90c
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-18 17:50:30 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+ 
+ int a3,a4,i,t;
+ for(i=0;i<3;i++)
+ {
+ scanf("%d %d",&a3,&a4);
+  if(a3>a4)
+   printf("%d",a3-a4);
+   else
+   printf("%d",a3-a4);
+ }
+}
