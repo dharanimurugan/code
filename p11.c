387beac2f4b8d7155b31b7cc334e49cd945351c3
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-23 15:27:52 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    char a[20];
+    scanf("%s",&a);
+    if(a[0]=='s'||a[0]=='s')
+    {
+        printf("yes...holiday");
+    }
+    else
+    {
+        printf("not...a holiday");
+    }
+    getch();
+}
+
