e7381dff028e14cbefb5159171ee9e45e9cd0d81
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-23 13:22:35 +0530

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[40];
+int i,c=0;
+scanf("%c",&a);
+for(i=0;i<a[i];i++)
+{
+for(i=0;i<a[i];i++)
+{
+if(a[i]=='V')
+c=c+5;
+else if(a[i]=='I')
+c=c+1;
+else if(a[i]=='X')
+c=c+10;
+}
+printf("%d\n",c);
+getch();
+}
