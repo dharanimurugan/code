66a0ec6c025364ae7a2eb59763f82da13e8922ed
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-24 15:28:05 +0530

    Create p28.c

---
+++ p28.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+   char s[100];
+   int i,l,flag=0;
+   gets(s);
+   l=strlen(s);
+   for(i=0;i<l;i++)
+   {
+       if(s[i]==' ')
+       {
+           l--;
+       s[i]=s[i+1];
+       printf("%s",s);
+       
+       }
+   }
+    getch();
+}
