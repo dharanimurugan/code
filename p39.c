dd449d9d5b092e05970594dbaf7fa505e31b633d
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-02-27 21:03:29 +0530

    Create p39.c

---
+++ p39.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+	int m,i,p=2,result=1;
+	scanf("%d",&m);
+	for(i=1;i<=m;i++)
+	{ult=result*p;
+	    if(result==m)
+	    {
+	        printf("yes");
+	        break;
+	    }
+	    else
+	    {
+	        continue;
+	    }
+	}
+	if(result!=m)
+	{
+	    printf("no");
+	}
+	
+	getch();
+}
