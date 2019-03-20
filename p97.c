a39d27dc4cb9eb6619234ca5bfde76bff6f99c0e
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-03-20 18:00:20 +0530

    Create p97.c

---
+++ p97.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(void)
+{
+    int l,r,i,sum=0;
+    scanf("%d %d",&l,&r);
+    for(i=l;i<=r;i++)
+    {
+        if(i%2==1)
+        {
+            sum=sum+i;
+        }
+    }
+    printf("%d\n",sum);
+}
+getch();
+}
