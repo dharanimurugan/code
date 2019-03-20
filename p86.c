f1e33e6de6cf474eea43e06091dd0d98af007342
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-03-20 17:47:39 +0530

    Create p86.c

---
+++ p86.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i,res=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		res=res^array[i];
+	}
+	printf("%d\n",res);
+	getch();
+}
