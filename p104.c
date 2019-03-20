df97029658407bfb414650d10a5283bd8b5e3aa6
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-03-20 18:10:44 +0530

    Create p104.c

---
+++ p104.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(void) 
+{
+	int a[10],n,sum=0,i,j;
+	scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0,j=i+1;i<n,j<n;i++,j++)
+    {
+        sum=sum+a[i]+a[j];
+    }
+    printf("%d\n",sum);
+	getch();
+}
