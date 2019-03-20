a1d7edc96f4d036b9147f12993157910550f7a35
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-03-20 17:08:52 +0530

    Create p62.c

---
+++ p62.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+	int n,i,f;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			f=n/i;
+			if(x%2==1)
+			{
+				printf("%d\n",i);
+				break;
+			}
+		}
+		
+	}
+getch();
+}
