c0d7159ce30d5f71079ac53be03ec70b8ff6af20
dharanimurugan <47447231+dharanimurugan@users.noreply.github.com>
2019-03-24 19:14:19 +0530

    Create p159.c

---
+++ p159.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() 
+{
+	int n,m,i,rem,pro,a[20],count=0,count1=0;
+	scanf("%d %d",&n,&m);
+	pro=n*m;
+	while(pro!=0)
+	{
+		rem=pro%2;
+		a[i]=rem;
+		i++;
+		count=count+1;
+		pro=pro/2;
+	}
+	for(i=0;i<count;i++)
+	{
+		if(a[i]==1)
+		{
+			count1=count1+1;
+		}
+	}
+	printf("%d\n",count1);
+	getch();
+}
